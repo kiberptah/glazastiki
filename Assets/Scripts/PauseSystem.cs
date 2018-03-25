bc70237f47bfce700dba039ee7e115be0e77b1b6
kiberptah <36883507+kiberptah@users.noreply.github.com>
2018-03-25 23:16:51 +0300

    minor update
    Работаю над меню.

---
+++ Assets/Scripts/PauseSystem.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class PauseSystem : MonoBehaviour {
+
+    public Transform canvas;
+
+    public Component spawnSystem;
+
+    bool isPaused = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            PauseGame(isPaused);
+        }
+	}
+
+    public void PauseGame(bool isPaused)
+    {      
+        canvas.gameObject.SetActive(isPaused);
+        gameObject.GetComponent<SpawnSystem>().enabled = !isPaused;
+    }
+
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
--- Assets/Scripts/PixelPerfectSize.cs
+++ Assets/Scripts/PixelPerfectSize.cs
@@ -8,6 +8,8 @@ public class PixelPerfectSize : MonoBehaviour {
     public float height;
     public float width;
 
+    private int ppu = 32;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -17,10 +19,18 @@ public class PixelPerfectSize : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        
+        if (Screen.height <= 700)
+        {
+            ppu = 24;
+        }
+        if (Screen.height > 700)
+        {
+            ppu = 32;
+        }
+
         height = Screen.height;
         width = Screen.width;
 
-        theCamera.orthographicSize = height / 32f * 0.5f;
+        theCamera.orthographicSize = height / ppu * 0.5f;
     }
 }
