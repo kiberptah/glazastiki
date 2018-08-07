7b9066269ac67ac74a77a108dfc5b2d66c6ac3e7
St. Zol <zzaggrruzz@yandex.ru>
2018-08-07 03:54:36 +0300

    VISION LINES
    Теперь можно нажать на F1 и юниты соединятся ааааа

---
+++ Assets/Scripts/DrawLines.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+// Данный скрипт нужен для дебага линий и для хранения глобальной переменной ВКЛ/ВЫКЛ отображение линий взгляда
+public class DrawLines : MonoBehaviour {
+
+    public bool ShowVisionLines;
+    public Text visionInfo;
+
+    void Start ()
+    {
+        ShowVisionLines = false;
+        visionInfo.text = "";
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.F1))
+        {
+            ShowVisionLines = !ShowVisionLines;
+            //Draw();
+        }
+        if (ShowVisionLines)
+        {
+            visionInfo.text = "Vision Lines Enabled";
+        }
+        else
+        {
+            visionInfo.text = "";
+        }
+	}
+
+    void Draw()
+    {     
+        List<GameObject> allUnits = new List<GameObject>();
+        allUnits.AddRange(GameObject.FindGameObjectsWithTag("Units"));       
+
+        GameObject currentColor = gameObject.GetComponent<SpawnSystem>().objectToSpawn;
+        Color lineColor = currentColor.GetComponent<SpriteRenderer>().color;       
+        
+        foreach (GameObject unitA in allUnits)
+            {                
+                foreach (GameObject unitB in allUnits)
+                {
+                    if (unitA.name != unitB.name && unitA.GetComponent<SpriteRenderer>().color == currentColor.GetComponent<SpriteRenderer>().color)
+                    {
+                        Debug.DrawRay(unitA.transform.position, (unitB.transform.position - unitA.transform.position), lineColor, 1);                         
+                    }
+                }
+            }    
+    }
+}
---
+++ Assets/Scripts/Vision.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour {
+
+    //Color currentColor;
+    public GameObject gameSystems;
+
+    //bool lineDisplay;
+
+    void Start ()
+    {
+        if (transform.position.y != -100) // на y = -100 лежат префабы
+        {
+            //lineDisplay = false;
+            gameObject.GetComponent<LineRenderer>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (transform.position.y != -100) // на y = -100 лежат префабы
+        {           
+            DrawLines();                 
+        }
+    }
+
+    void DrawLines()
+    {
+        List<GameObject> allUnits = new List<GameObject>();
+        //currentColor = gameSystems.GetComponent<SpawnSystem>().objectToSpawn.GetComponent<SpriteRenderer>().color;
+
+        LineRenderer connector;      
+        connector = GetComponent<LineRenderer>();
+
+        connector.enabled = (gameObject.GetComponent<SpriteRenderer>().color == gameSystems.GetComponent<SpawnSystem>().objectToSpawn.GetComponent<SpriteRenderer>().color) 
+            && gameSystems.GetComponent<DrawLines>().ShowVisionLines; //если цвета ок и режим включен то показываем
+
+        connector.positionCount = 0;
+
+        connector.startColor = connector.endColor = gameObject.GetComponent<SpriteRenderer>().color;
+
+
+        allUnits.AddRange(GameObject.FindGameObjectsWithTag("Units"));
+
+        //connector.positionCount = allUnits.Count + 1;
+        
+        Vector3[] unitCoords = new Vector3[allUnits.Count];
+
+
+        connector.positionCount++;
+        connector.SetPosition(0, transform.position);
+        int i = 1;
+        foreach (GameObject unit in allUnits)
+        {
+            if (unit.transform.GetComponent<SpriteRenderer>().color != gameObject.GetComponent<SpriteRenderer>().color)
+            {
+                connector.positionCount++;
+                connector.SetPosition(i, unit.transform.position);
+                Debug.Log("posiition " + i + " = " + connector.GetPosition(i));
+                ++i;
+
+                connector.positionCount++;
+                connector.SetPosition(i, transform.position);
+                Debug.Log("RETURN: position " + i + " = " + connector.GetPosition(i));
+                ++i;
+            }
+        }    
+        
+        /*
+        int i = 0;
+        foreach (GameObject unit in allUnits)
+        {
+            if (unit.transform.GetComponent<SpriteRenderer>().color != currentColor)
+            {
+                unitCoords[i] = unit.transform.position;
+                //Debug.Log(unitCoords[i]);
+
+                ++i;
+           }        
+        }
+
+        connector.positionCount++;
+        connector.SetPosition(0, transform.position);
+        for (int a = 1; a <= allUnits.Count; a += 2)
+        {
+            connector.positionCount++;
+            connector.SetPosition(a, unitCoords[a-1]);
+            Debug.Log("posiition " + a + " = " + connector.GetPosition(a));
+
+            connector.positionCount++;
+            connector.SetPosition((a + 1), transform.position);
+            Debug.Log("RETURN: position " + (a + 1) + " = " + connector.GetPosition(a+1));
+        }
+        */
+    }
+}
